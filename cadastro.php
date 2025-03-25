c6ac3b1f01a26c1171dc68f04e90710566815b7f
Libonico <pedrorochaliboni@hotmail.com>
2025-03-24 22:23:24 -0300

    💄 "Reformulada" a tela de cadastro
    Acrescentadas informações de CPF e Cartão SUS. Reformulado layout dos inputs e labels da página.

--- cadastro.php
+++ cadastro.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>cadastro</title>
 </head>
 <body>
     <?php
-    echo $_GET['bomeUsuario'];
+    echo $_GET['nomeUsuario'];
     echo $_GET['nascimento'];
     echo $_GET['email'];
     echo $_GET['senha'];
     echo $_GET['senhaConfirma'];
+    echo $_GET['cartaoSus'];
+    echo $_GET['cpf'];
     ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +9,8 @@
   <?php
 echo $_GET['email'];
 echo $_GET['senha'];
+echo $_GET['cartaoSus'];
+echo $_GET['cpf'];
 ?>
 </body>
 </html>
