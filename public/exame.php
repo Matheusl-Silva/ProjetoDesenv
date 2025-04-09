6bc1115d07788a08b759882ac7e5e3c3cb51022a
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2025-04-09 19:48:36 -0300

    ⭐ criação do arquivo exame.php

---
+++ public/exame.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Cadastro de exame</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form action="#">
+                    <label for="registro" class="form-label">Registro:</label>
+                    <input type="text" class="form-control">
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
