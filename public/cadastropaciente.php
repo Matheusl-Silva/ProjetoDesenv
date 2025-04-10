eb6d6316f9fe0004600e0b4b0a1f608a20454f73
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2025-04-09 21:44:15 -0300

    fix: ajuste css

--- public/cadastropaciente.php
+++ public/cadastropaciente.php
@@ -9,7 +9,7 @@
     <title>Cadastro de exame</title>
 </head>
 
-<body>
+<body class="bg-info-subtle">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
