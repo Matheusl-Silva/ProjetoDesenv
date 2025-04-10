f9dec49be346eb948241c4b66b31cab555153478
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2025-04-09 21:11:45 -0300

    ⭐feat: adição do input medicamento

--- public/cadastropaciente.php
+++ public/cadastropaciente.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="../css/cadastropaciente.css">
     <title>Cadastro de exame</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row justify-content-center">
@@ -19,10 +21,14 @@
                     <input type="date" class="form-control" id="data">
 
                     <p class="form-label">Período:</p>
-                    <input type="radio" id="matutino" class="form-check-input" name="periodo" value="matutino" checked>
-                    <label for="matutino" class="form-check-label">Matutino</label>
-                    <input type="radio" id="noturno" class="form-check-input" name="periodo" value="noturno">
-                    <label for="noturno" class="form-check-label">Noturno</label>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" id="matutino" class="form-check-input" name="periodo" value="matutino" checked>
+                        <label for="matutino" class="form-check-label">Matutino</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" id="noturno" class="form-check-input" name="periodo" value="noturno">
+                        <label for="noturno" class="form-check-label">Noturno</label>
+                    </div>
 
                     <label for="nome" class="form-label">Nome completo:</label>
                     <input type="text" class="form-control" id="nome" name="nome">
@@ -35,13 +41,31 @@
 
                     <label for="email" class="form-label">E-mail para contato:</label>
                     <input type="email" id="email" class="form-control" name="email">
-                    
-                    <label for="nomemae" class="form-label">Nome da mãe:</label>
-                    <input type="text" class="form-control" id="nomemae" name="nomemae">
+
+                    <label for="nomeMae" class="form-label">Nome da mãe:</label>
+                    <input type="text" class="form-control" id="nomeMae" name="nomeMae">
+
+                    <p class="form-label">Toma algum medicamento contínuo? Se sim, qual?</p>
+                    <div class="form-check">
+                        <input type="radio" id="medNao" class="form-check-input" name="tomaMedicamento" value="medNao">
+                        <label for="medNao" class="form-check-label">Não</label>
+                    </div>
+                    <div class="form-check container">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <input type="radio" id="medSim" class="form-check-input" name="tomaMedicamento" value="medSim">
+                                <label for="medSim" class="form-check-label">Sim:</label>
+                            </div>
+                            <div class="col-md" style="padding:0">
+                                <input type="text" class="form-control" id="medicamento" name="medicamento">
+                            </div>
+                        </div>
+                    </div>
 
                 </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
