ba7d20c7a33b5a924daaa32e436bd47d6b5f5fcf
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2025-04-11 20:45:00 -0300

    ⭐feat: adição de tela de usuário

---
+++ public/home-usuario.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/home-usuario.css">
+    <title>Página principal</title>
+</head>
+
+<body class="bg-info-subtle">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary-subtle sticky-top">
+            <div class="container">
+                <img src="img/health.png" class="imagemSize" alt="">
+                <a class="navbar-brand mx-auto text-primary" href="#">Projeto Saude</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="public/cadastro.php">Cadastrar-se</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="public/login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">Conheça o Projeto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#team">Quem Somos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row justify-content-evenly row1">
+                <div class="card col-md-5">
+                    <div class="col-1" style="background-color: red">
+                        <div>
+                            <p>👤</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card col-md-5">
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
