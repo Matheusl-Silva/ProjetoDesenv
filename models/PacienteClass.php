a3ddee5e4f6919b5f96b824917681ed652f2129d
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2025-05-28 22:20:25 -0300

    adicao da funcao para rendereizar tabela de paceintes

--- models/PacienteClass.php
+++ models/PacienteClass.php
@@ -1,20 +1,24 @@
 <?php
 class Paciente extends Pessoa{
     private $registro;
-    private $data; //Precisa?
     private $periodo;
     private $dataNasc;
     private $fone;
     private $nomeMae;
 
-	public function __construct($nome, $email, $registro, $data, $periodo, $dataNasc, $fone, $nomeMae) {
+	private $mysqli;
+
+	public function __construct($nome, $email, $registro, $periodo, $dataNasc, $fone, $nomeMae) {
         parent::__construct($nome, $email);
 		$this->registro = $registro;
-		$this->data = $data;
 		$this->periodo = $periodo;
 		$this->dataNasc = $dataNasc;
 		$this->fone = $fone;
 		$this->nomeMae = $nomeMae;
+
+		require_once __DIR__. "/../database/conexaoClass.php";
+		$bd = new Conexao();
+		$this->mysqli = $bd->getConexao();
 	}
 
 	public function getRegistro() {
@@ -23,14 +27,6 @@ class Paciente extends Pessoa{
 
 	public function setRegistro($valor) {
 		$this->registro = $valor;
-	}
-
-	public function getData() {
-		return $this->data;
-	}
-
-	public function setData($valor) {
-		$this->data = $valor;
 	}
 
 	public function getPeriodo() {
@@ -63,5 +59,46 @@ class Paciente extends Pessoa{
 
 	public function setNomeMae($valor) {
 		$this->nomeMae = $valor;
+	}
+
+	public function listarPacientes(){
+		$sql = "SELECT * FROM pacientes ORDER BY id";
+		$resultado = $this->mysqli->query($sql);
+		$pacientes = [];
+
+		if($resultado){
+			while($linha = $resultado ->fetch_assoc()){
+				$pacientes[] = $linha;
+			}
+		}
+
+		return $pacientes;
+	}
+
+	public function renderizarTabelaPaciente(){
+		$listaPacientes = $this->listarPacientes();
+		$html = '<div class="table-responsive mt-3">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Período</th>
+                                <th>Data de nascimento</th>
+                                <th>Telefone</th>
+                                <th>Nome da mãe</th>
+                            </tr>
+                        </thead>
+                        <tbody>';
+
+		foreach ($listaPacientes as $paciente){
+			$html.= '<td>' . htmlspecialchars($paciente["id"]) . '</td>
+					<td>' . htmlspecialchars($paciente["nome"]) . '</td>
+					<td>' . htmlspecialchars($paciente["periodo"]) . '</td>
+					<td>' . htmlspecialchars($paciente["data_nascimento"]) . '</td>
+					<td>' . htmlspecialchars($paciente["telefone"]) . '</td>
+					<td>' . htmlspecialchars($paciente["nome_mae"]);
+		}
 	}
 }
