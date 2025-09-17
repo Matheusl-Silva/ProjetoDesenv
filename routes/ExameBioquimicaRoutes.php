3931de19f42912a0a039470fd49f1668f006e516
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2025-09-17 20:23:09 -0300

    ⭐ feat: Rota de exclusão de Bioquimica no PHP

--- routes/ExameBioquimicaRoutes.php
+++ routes/ExameBioquimicaRoutes.php
@@ -8,6 +8,13 @@ return function (Router $router) {
 
         $exameBioquimicaController = new exameBioquimicaController();
         $exameBioquimicaController->VisualizarExame($id);
+    });
 
+    $router->delete('/exameBio/{id}', function ($id) {
+        $exameBioquimicaController = new ExameBioquimicaController();
+        $exameBioquimicaController->excluir($id);
+
+        header('Location: /exames?paciente=' . $id);
+        exit;
     });
 };
