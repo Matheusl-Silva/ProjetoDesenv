9f5a19b1f246a48da84963e1c3971e5c392f8f00
MatheusKudlake <178607990+MatheusKudlake@users.noreply.github.com>
2025-10-07 19:16:15 -0300

    🐛 fix: Correção na conexão com banco de dados

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "laboratorio",
 });
 
-/*db.connect((err) => {
+db.connect((err) => {
   if (err) {
     console.log("Erro de conexão: " + err);
     return;
   }
   console.log("Conexão com banco de dados estabelecida");
-});*/
+});
 
 module.exports = db;
