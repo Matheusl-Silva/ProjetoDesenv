c69a4470f9aaa490e8a0c98378469122ed87665a
Matheusl-Silva <matleandrosilva@gmail.com>
2026-03-17 20:57:40 -0300

    🐛 fix: ajuste no replace do ","
    ajustado o replace na mmodel para poder incluir numero com ,

--- models/ExameHemato.php
+++ models/ExameHemato.php
@@ -44,132 +44,132 @@ class ExameHemato
     public function getHemacia()
     {return $this->hemacia;}
     public function setHemacia($hemacia)
-    {$this->hemacia = $hemacia;}
+    {$this->hemacia = is_string($hemacia) ? str_replace(',', '.', $hemacia) : $hemacia;}
 
     public function getHemoglobina()
     {return $this->hemoglobina;}
     public function setHemoglobina($hemoglobina)
-    {$this->hemoglobina = $hemoglobina;}
+    {$this->hemoglobina = is_string($hemoglobina) ? str_replace(',', '.', $hemoglobina) : $hemoglobina;}
 
     public function getHematocrito()
     {return $this->hematocrito;}
     public function setHematocrito($hematocrito)
-    {$this->hematocrito = $hematocrito;}
+    {$this->hematocrito = is_string($hematocrito) ? str_replace(',', '.', $hematocrito) : $hematocrito;}
 
     public function getVcm()
     {return $this->vcm;}
     public function setVcm($vcm)
-    {$this->vcm = $vcm;}
+    {$this->vcm = is_string($vcm) ? str_replace(',', '.', $vcm) : $vcm;}
 
     public function getHcm()
     {return $this->hcm;}
     public function setHcm($hcm)
-    {$this->hcm = $hcm;}
+    {$this->hcm = is_string($hcm) ? str_replace(',', '.', $hcm) : $hcm;}
 
     public function getChcm()
     {return $this->chcm;}
     public function setChcm($chcm)
-    {$this->chcm = $chcm;}
+    {$this->chcm = is_string($chcm) ? str_replace(',', '.', $chcm) : $chcm;}
 
     public function getRdw()
     {return $this->rdw;}
     public function setRdw($rdw)
-    {$this->rdw = $rdw;}
+    {$this->rdw = is_string($rdw) ? str_replace(',', '.', $rdw) : $rdw;}
 
     public function getLeucocitos()
     {return $this->leucocitos;}
     public function setLeucocitos($leucocitos)
-    {$this->leucocitos = $leucocitos;}
+    {$this->leucocitos = is_string($leucocitos) ? str_replace(',', '.', $leucocitos) : $leucocitos;}
 
     public function getNeutrofilos()
     {return $this->neutrofilos;}
     public function setNeutrofilos($neutrofilos)
-    {$this->neutrofilos = $neutrofilos;}
+    {$this->neutrofilos = is_string($neutrofilos) ? str_replace(',', '.', $neutrofilos) : $neutrofilos;}
 
     public function getBlastos()
     {return $this->blastos;}
     public function setBlastos($blastos)
-    {$this->blastos = $blastos;}
+    {$this->blastos = is_string($blastos) ? str_replace(',', '.', $blastos) : $blastos;}
 
     public function getPromielocitos()
     {return $this->promielocitos;}
     public function setPromielocitos($promielocitos)
-    {$this->promielocitos = $promielocitos;}
+    {$this->promielocitos = is_string($promielocitos) ? str_replace(',', '.', $promielocitos) : $promielocitos;}
 
     public function getMielocitos()
     {return $this->mielocitos;}
     public function setMielocitos($mielocitos)
-    {$this->mielocitos = $mielocitos;}
+    {$this->mielocitos = is_string($mielocitos) ? str_replace(',', '.', $mielocitos) : $mielocitos;}
 
     public function getMetamielocitos()
     {return $this->metamielocitos;}
     public function setMetamielocitos($metamielocitos)
-    {$this->metamielocitos = $metamielocitos;}
+    {$this->metamielocitos = is_string($metamielocitos) ? str_replace(',', '.', $metamielocitos) : $metamielocitos;}
 
     public function getBastonetes()
     {return $this->bastonetes;}
     public function setBastonetes($bastonetes)
-    {$this->bastonetes = $bastonetes;}
+    {$this->bastonetes = is_string($bastonetes) ? str_replace(',', '.', $bastonetes) : $bastonetes;}
 
     public function getSegmentados()
     {return $this->segmentados;}
     public function setSegmentados($segmentados)
-    {$this->segmentados = $segmentados;}
+    {$this->segmentados = is_string($segmentados) ? str_replace(',', '.', $segmentados) : $segmentados;}
 
     public function getEosinofilos()
     {return $this->eosinofilos;}
     public function setEosinofilos($eosinofilos)
-    {$this->eosinofilos = $eosinofilos;}
+    {$this->eosinofilos = is_string($eosinofilos) ? str_replace(',', '.', $eosinofilos) : $eosinofilos;}
 
     public function getBasofilos()
     {return $this->basofilos;}
     public function setBasofilos($basofilos)
-    {$this->basofilos = $basofilos;}
+    {$this->basofilos = is_string($basofilos) ? str_replace(',', '.', $basofilos) : $basofilos;}
 
     public function getLinfocitos()
     {return $this->linfocitos;}
     public function setLinfocitos($linfocitos)
-    {$this->linfocitos = $linfocitos;}
+    {$this->linfocitos = is_string($linfocitos) ? str_replace(',', '.', $linfocitos) : $linfocitos;}
 
     public function getLinfocitosAtipicos()
     {return $this->linfocitosAtipicos;}
     public function setLinfocitosAtipicos($linfocitosAtipicos)
-    {$this->linfocitosAtipicos = $linfocitosAtipicos;}
+    {$this->linfocitosAtipicos = is_string($linfocitosAtipicos) ? str_replace(',', '.', $linfocitosAtipicos) : $linfocitosAtipicos;}
 
     public function getMonocitos()
     {return $this->monocitos;}
     public function setMonocitos($monocitos)
-    {$this->monocitos = $monocitos;}
+    {$this->monocitos = is_string($monocitos) ? str_replace(',', '.', $monocitos) : $monocitos;}
 
     public function getMieloblastos()
     {return $this->mieloblastos;}
     public function setMieloblastos($mieloblastos)
-    {$this->mieloblastos = $mieloblastos;}
+    {$this->mieloblastos = is_string($mieloblastos) ? str_replace(',', '.', $mieloblastos) : $mieloblastos;}
 
     public function getOutrasCelulas()
     {return $this->outrasCelulas;}
     public function setOutrasCelulas($outrasCelulas)
-    {$this->outrasCelulas = $outrasCelulas;}
+    {$this->outrasCelulas = is_string($outrasCelulas) ? str_replace(',', '.', $outrasCelulas) : $outrasCelulas;}
 
     public function getCelulasLinfoides()
     {return $this->celulasLinfoides;}
     public function setCelulasLinfoides($celulasLinfoides)
-    {$this->celulasLinfoides = $celulasLinfoides;}
+    {$this->celulasLinfoides = is_string($celulasLinfoides) ? str_replace(',', '.', $celulasLinfoides) : $celulasLinfoides;}
 
     public function getCelulasMonocitoides()
     {return $this->celulasMonocitoides;}
     public function setCelulasMonocitoides($celulasMonocitoides)
-    {$this->celulasMonocitoides = $celulasMonocitoides;}
+    {$this->celulasMonocitoides = is_string($celulasMonocitoides) ? str_replace(',', '.', $celulasMonocitoides) : $celulasMonocitoides;}
 
     public function getPlaquetas()
     {return $this->plaquetas;}
     public function setPlaquetas($plaquetas)
-    {$this->plaquetas = $plaquetas;}
+    {$this->plaquetas = is_string($plaquetas) ? str_replace(',', '.', $plaquetas) : $plaquetas;}
 
     public function getVolumePlaquetarioMedio()
     {return $this->volumePlaquetarioMedio;}
     public function setVolumePlaquetarioMedio($volumePlaquetarioMedio)
-    {$this->volumePlaquetarioMedio = $volumePlaquetarioMedio;}
+    {$this->volumePlaquetarioMedio = is_string($volumePlaquetarioMedio) ? str_replace(',', '.', $volumePlaquetarioMedio) : $volumePlaquetarioMedio;}
 
     public function getData()
     {return $this->data;}
